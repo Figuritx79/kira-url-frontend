9c93ec981f75d1037994d65a0042125e9540cfa0
Enrique Gonzalez <gonherenrique@gmail.com>
2026-07-09 21:50:33 -0600

    Type checking ready

--- src/i18n/manager.ts
+++ src/i18n/manager.ts
@@ -1,19 +1,21 @@
+import type { I18n } from "vue-i18n";
+
 const localeModules = import.meta.glob('./locales/*.json')
-let _i18n = null;
+let _i18n: I18n| null = null;
 const I18nManager = {
   get supportedLocales() {
     return Object.keys(localeModules)
       .map((file) => file.match(/\.\/locales\/([^/]+)\.json$/)?.[1])
       .filter(Boolean)
   },
-  init(i18nInstance) {
+  init(i18nInstance: I18n) {
     if (!i18nInstance?.global?.locale) {
       throw new Error("I18nManager.init: invalid i18n instance");
     }
     _i18n = i18nInstance;
   },
 
-  async setLocale(newLocale) {
+  async setLocale(newLocale: string) {
     if (!_i18n) throw new Error("I18nManager.setLocale called before init");
 
     const current = _i18n.global.locale.value;
